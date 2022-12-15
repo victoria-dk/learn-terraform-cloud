f8b38cdba0584df3cd15ed0ef381cd454a30f2cb
vnielsen1-admin <victoria-dk@hotmail.com>
2022-12-15 15:44:35 +0100

    Remove cloud block

--- terraform.tf
+++ terraform.tf
@@ -1,13 +1,13 @@
 terraform {
-
+/*
   cloud {
-    organization = "organization-name"
+    organization = "viclabs-org"
 
     workspaces {
       name = "learn-terraform-cloud"
     }
   }
-
+*/
   required_providers {
     aws = {
       source  = "hashicorp/aws"
